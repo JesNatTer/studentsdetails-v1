a4475c7fba7d73afe7f8ce1980e8ac6e221e2856
Jesse Terblanche <jessenterblanche@gmail.com>
2021-07-23 09:55:15 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import sqlite3
+from flask import Flask, render_template
+
+
+def init_sqlite_db():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+    conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+    print("Table created successfully")
+    conn.close()
+    init_sqlite_db()
+
+
+app = Flask(__name__)
+
+
+@app.route('/enter-new/')
+def enter_new_student():
+    return render_template('student.html')
